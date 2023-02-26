0df543ad5839b0eb0b07a416be8934932a66a9b6
Piotr Niewinski <p.niewinski@edrone.me>
2023-02-26 22:35:11 +0100

    deleted header

--- hpmser/search_function.py
+++ hpmser/search_function.py
@@ -1,37 +1,3 @@
-"""
-
- 2020 (c) piteren
-
-    hpmser - hyperparameters searching function
-        > searches hyperparameters space to MAXIMIZE the SCORE of func
-
-        MAXIMIZE the SCORE == find points cluster with:
-         - high num of points in a small distance (dst)
-         - high max smooth_score
-         - high lowest score
-
-         policy of sampling the space is crucial (determines the speed, top result and convergence)
-         - fully random sampling is slow, wastes a lot of time and computing power
-         - too aggressive sampling may undersample the space and miss the real MAX
-
-    to setup search:
-    1. having some FUNCTION:
-        - some parameters need to be optimized
-        - some parameters may be fixed / constant
-        - if function accepts 'device' or 'devices' it should be type of DevicesPypaq (check pypaq.mpython.devices),
-          it will be used by hpmser to put proper device for each function call
-        - returns a dict with 'score' or just a value (score)
-        There are two parameters of FUNCTION that may be used by hpmser:
-            - 'devices' (type of DevicesPypaq - check pypaq.mpython.devices) -> hpmser will send devices to FUNCTION
-            - 'hpmser_mode' -> will be set to True by hpmser
-    2. define PSDD - dictionary with parameters to be optimized and the space to search in (check pypaq.pms.pasap.PaSpa)
-    3. import hpmser function into your script, and run it with:
-        - func << FUNCTION
-        - func_psdd << PSDD
-        - func_const << dictionary of parameters that have to be given but should be fixed / constant durring optimization
-        - .. other params configure hpmser algorithm itself
-"""
-
 import os
 from pypaq.lipytools.printout import stamp
 from pypaq.lipytools.files import prep_folder
