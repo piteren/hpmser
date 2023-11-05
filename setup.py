539faf797d211e723ff7ad21afc925efe57be69e
Piotr Niewinski <me@piotrniewinski.com>
2023-11-05 20:03:14 +0100

    new ver

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_requirements():
 
 setup(
     name=               'hpmser',
-    version=            'v2.1.0',
+    version=            'v2.2.0',
     url=                'https://github.com/piteren/hpmser.git',
     author=             'Piotr Niewinski',
     author_email=       'pioniewinski@gmail.com',
