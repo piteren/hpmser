d1c04ee17b7561f724df1cfce8668d7396eab1db
pionie <piotr.niewinski@a5labs.co>
2026-01-20 21:11:13 +0100

    Update version to v2.2.1

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
 from setuptools import setup, find_packages
 
-# reads requirements
-def get_requirements():
-    with open('requirements.txt') as file:
-        lines = [l[:-1] if l[-1]=='\n' else l for l in file.readlines()]
-        return lines
+from pypaq.lipytools.files import get_requirements
 
 
 setup(
     name=               'hpmser',
-    version=            'v2.2.0',
+    version=            'v2.2.1',
     url=                'https://github.com/piteren/hpmser.git',
     author=             'Piotr Niewinski',
     author_email=       'pioniewinski@gmail.com',
