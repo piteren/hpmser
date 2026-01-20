5045aab37e0d6ffe9004de1a3dd72cd50359b22f
pionie <piotr.niewinski@a5labs.co>
2026-01-20 21:14:37 +0100

    Update version to v2.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pypaq.lipytools.files import get_requirements
 
 setup(
     name=               'hpmser',
-    version=            'v2.2.1',
+    version=            'v2.2.2',
     url=                'https://github.com/piteren/hpmser.git',
     author=             'Piotr Niewinski',
     author_email=       'pioniewinski@gmail.com',
